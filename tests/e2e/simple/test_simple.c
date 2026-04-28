d2315df59e7c27fc1fcde7d77f1c3f8065c712e2
Thomas Chiantia <thomas@altronix.com>
2026-04-28 14:32:25 -0400

    clang format on e2e simple test

--- tests/e2e/simple/test_simple.c
+++ tests/e2e/simple/test_simple.c
@@ -4,27 +4,27 @@
 void
 setUp(void)
 {
-  Mocksimple_dep_Init();
+    Mocksimple_dep_Init();
 }
 
 void
 tearDown(void)
 {
-  Mocksimple_dep_Verify();
-  Mocksimple_dep_Destroy();
+    Mocksimple_dep_Verify();
+    Mocksimple_dep_Destroy();
 }
 
 void
 test_mock_returns_canned_value(void)
 {
-  simple_dep_get_ExpectAndReturn(42);
-  TEST_ASSERT_EQUAL_INT(42, simple_dep_get());
+    simple_dep_get_ExpectAndReturn(42);
+    TEST_ASSERT_EQUAL_INT(42, simple_dep_get());
 }
 
 int
 main(void)
 {
-  UNITY_BEGIN();
-  RUN_TEST(test_mock_returns_canned_value);
-  return UNITY_END();
+    UNITY_BEGIN();
+    RUN_TEST(test_mock_returns_canned_value);
+    return UNITY_END();
 }
